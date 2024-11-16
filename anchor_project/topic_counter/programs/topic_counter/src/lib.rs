69d3e39964f13944e691c045d5c78d80b37475b0
akaMiWP <bangkokappdevelopment@gmail.com>
2024-11-16 11:39:50 +0000

    Added Accounts component with a normal account in struct + its initialize test

--- anchor_project/topic_counter/programs/topic_counter/src/lib.rs
+++ anchor_project/topic_counter/programs/topic_counter/src/lib.rs
@@ -6,11 +6,30 @@ declare_id!("7PNBCvkBFzRfoGEuZhnPzT58YNyPpGkNBxdJpjseXJRc");
 pub mod topic_counter {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<InitializeTopic>) -> Result<()> {
         msg!("Greetings from: {:?}", ctx.program_id);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitializeTopic<'info> {
+    #[account(mut)]
+    pub topic_owner: Signer<'info>,
+
+    #[account(
+        init,
+        payer = topic_owner,
+        space = 8 + 32 + 32 + 200,
+    )]
+    pub topic_account: Account<'info, Topic>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Topic {
+    pub topic_author: Pubkey,
+    pub title: [u8; 32],
+    pub content: [u8; 200],
+}
--- anchor_project/topic_counter/tests/topic_counter.ts
+++ anchor_project/topic_counter/tests/topic_counter.ts
@@ -1,16 +1,43 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { TopicCounter } from "../target/types/topic_counter";
+import { PublicKey } from '@solana/web3.js';
 
 describe("topic_counter", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env()
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.TopicCounter as Program<TopicCounter>;
 
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const user = anchor.web3.Keypair.generate()
+    const topicAccount = anchor.web3.Keypair.generate()
+
+    // const topicTitle = Buffer.from("My Topic", "utf-8")
+    // const contentTitle = Buffer.from("My Content", "utf-8")
+    // const [topicKey, topicBump] = PublicKey.findProgramAddressSync([
+    //   topicOwner.publicKey.toBuffer(),
+    //   topicTitle,
+    //   contentTitle
+    // ], program.programId)
+
+    await airdrop(provider.connection, user.publicKey)
+
+    const tx = await program.methods
+    .initialize()
+    .accounts({
+      topicOwner: user.publicKey,
+      topicAccount: topicAccount.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId,
+    })
+    .signers([user, topicAccount])
+    .rpc({skipPreflight: true})
+   
     console.log("Your transaction signature", tx);
   });
 });
+
+async function airdrop(connection: any, address: any, amount = 1000000000) {
+  await connection.confirmTransaction(await connection.requestAirdrop(address, amount), "confirmed");
+}
